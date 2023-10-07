1596fcae42f108a82602096b6fdd9d1b380b799e
Roth <rothvann2000@gmail.com>
2023-10-07 06:27:57 +0000

    Fixed Types.h for real

--- psychopomp/Types.h
+++ psychopomp/Types.h
@@ -12,20 +12,20 @@ typedef size_t Domain;
 typedef size_t DomainId;
 typedef size_t Metric;
 
-struct AllocatorInput {
-  AllocatorOutput previousOutput;
-
-  // Vector contains metrics for each shard in the previous range. Sizes should
-  // match.
-  std::unordered_map<Metric, std::vector<int32_t>> metricVectorMap_;
-};
-
 struct AllocatorOutput {
   /*
   Each adjacent pair of elements is a range. Consider [0, 5, 15, 35], the pairs
   are: [0, 4] [5, 14] [15, 34] and the whole range is [0, 34]
   */
   std::vector<int64_t> shardRanges;
+};
+
+struct AllocatorInput {
+  AllocatorOutput previousOutput;
+
+  // Vector contains metrics for each shard in the previous range. Sizes should
+  // match.
+  std::unordered_map<Metric, std::vector<int32_t>> metricVectorMap_;
 };
 
 }  // namespace psychopomp
