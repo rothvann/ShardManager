3a9f05a0ab46f2205a1a9f04e9f3382421f7e881
reach.vann@gmail.com <reach.vann@gmail.com>
2023-10-07 02:54:32 +0000

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ git submodule update
 sudo apt install build-essential lld clang libssl-dev libfmt-dev linux-tools-common linux-tools-generic
 
 # Folly deps
-(cd externalDeps/folly && sudo ./build/fbcode_builder/getdeps.py install-system-deps --recursive)
+(cd externalDeps/folly && ./build/fbcode_builder/getdeps.py install-system-deps --recursive)
 
 # Buck install
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
