8a24004dcda4743d385e52411fad898abe40ac56
Roth <rothvann2000@gmail.com>
2023-10-30 23:15:59 +0000

    Implementation for AsyncServer

--- server_utils/AsyncServer.cpp
+++
@@ -1,3 +0,0 @@
-#include "server_utils/AsyncServer.h"
-
-namespace svc_discovery {}
--- server_utils/AsyncServer.h
+++ server_utils/AsyncServer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <folly/executors/CPUThreadPoolExecutor.h>
+#include <folly/Synchronized.h>
 #include <grpcpp/grpcpp.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -15,14 +16,67 @@ template <typename Service>
 class AsyncServer {
  public:
   AsyncServer(size_t numIoThreads, std::string serverAddress,
-              std::shared_ptr<HandlerManager<Service>> requestHandlerManager);
+              std::shared_ptr<HandlerManager<Service>> requestHandlerManager)
+      : numIoThreads_(numIoThreads),
+        serverAddress_(serverAddress),
+        ioThreadPool_(numIoThreads),
+        requestHandlerManager_(requestHandlerManager),
+        service_() {}
 
-  void run();
+  ~AsyncServer() {
+    server_->Shutdown();
+    for (auto& completionQueue : completionQueues_) {
+      completionQueue->Shutdown();
+    }
+
+    ioThreadPool_.join();
+  }
+
+  void run() {
+    for (size_t i = 0; i < numIoThreads; i++) {
+      ioThreadPool_.add([&]() {
+        void* tag;
+        bool ok;
+        // Use flag to end task
+        while (shouldRun_.copy()) {
+          auto& completionQueue = completionQueues_[i];
+          requestHandlerManager_->addHandler(service_, completionQueue);
+
+          completionQueue->Next(&tag, &ok);
+          if (!ok) {
+            requestHandlerManager_->removeHandler(tag);
+          } else {
+            requestHandlerManager_->process(tag);
+          }
+        }
+      });
+    }
+    server_ = serverBuilder_.BuildAndStart();
+  }
 
  private:
-  folly::CPUThreadPoolExecutor ioThreadPool_;
+  void init() {
+    {
+      auto runPtr = shouldRun_.wlock();
+      *runPtr = true;
+    }
+    builder_.AddListeningPort(server_address, InsecureServerCredentials());
+    builder.RegisterService(&service_);
+
+    for (size_t i = 0; i < numIoThreads; i++) {
+      completionQueues_.emplace_back(builder.AddCompletionQueue());
+    }
+  }
+
   std::string serverAddress_;
+  size_t numIoThreads;
+  Service service_;
+  folly::Synchronized<bool> shouldRun_;
+
+  grpc::ServerBuilder builder_;
+  folly::CPUThreadPoolExecutor ioThreadPool_;
   std::shared_ptr<HandlerManager<Service>> requestHandlerManager_;
-  std::vector<grpc::ServerCompletionQueue> completionQueues_;
+  std::vector<grpc::ServerCompletionQueue*> completionQueues_;
+  std::unique_ptr<Server> server_;
 };
 }  // namespace server_utils
