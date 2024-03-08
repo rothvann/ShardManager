e94fa549a64e0fa85ee0ecefb58c3bab39855d5f
Arya <arya.bastani23@gmail.com>
2024-03-07 21:52:55 -0500

    aws_utils not building

--- aws_utils/GetShardMap.cpp
+++ aws_utils/GetShardMap.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <string>
-#include <jsoncpp/json/json.h>
 
 #include <aws/core/Aws.h>
 #include <aws/dynamodb/DynamoDBClient.h>
 #include <aws/dynamodb/model/AttributeDefinition.h>
 #include <aws/dynamodb/model/GetItemRequest.h>
-// snippet-end:[dynamodb.cpp.get_item.inc]
-#include "dynamodb_samples.h"
 
 int main(int argc, char** argv) {
   Aws::SDKOptions options;
   Aws::InitAPI(options);
-
-  Json::Reader reader;
-  Json::Value obj;
 
   std::cout << "Greeter received: ";
 
